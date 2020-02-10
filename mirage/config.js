fe43f9a719f2565b670099dae5613b3d7161158a
Andrew Paterson <andrew@hyraxbio.co.za>
2020-02-10 13:38:51 +0200

    Fixed spelling error.

--- mirage/config.js
+++ mirage/config.js
@@ -16,7 +16,7 @@ export default function () {
   // Attempt 3
   // this.passthrough(`/markdown/markdown-file.md`);
 
-  // Works, but it would be great not to be able to use a relative path
+  // Works, but it would be better to be able to use a relative path
   // this.passthrough(`https://localhost:4200/markdown/markdown-file.md`);
 
   this.passthrough();
